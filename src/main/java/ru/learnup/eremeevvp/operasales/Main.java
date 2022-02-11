5fa582c90ec9e11d8ff24a1364b7b9a6a94d930c
v.eremeev <vladimirt345@gmail.com>
2022-02-11 19:48:57 +0300

    Del comments

--- src/main/java/ru/learnup/eremeevvp/operasales/Main.java
+++ src/main/java/ru/learnup/eremeevvp/operasales/Main.java
@@ -11,11 +11,11 @@ import ru.learnup.eremeevvp.operasales.service.TicketService;
 public class Main {
     public static void main(String[] args) {
         final ConfigurableApplicationContext ctx = SpringApplication.run(Main.class);
-       // ctx.getBean(PremierList.class).addPremier();
         ctx.getBean(PremierList.class).addPremier();
-       // System.out.println(ctx.getBean(PremierList.class).removePremier());
-       // System.out.println(ctx.getBean(PremierList.class).showAllPremier());
-       // System.out.println(ctx.getBean(PremierList.class).showOnePremier("Опера2"));
+        ctx.getBean(PremierList.class).addPremier();
+        System.out.println(ctx.getBean(PremierList.class).removePremier());
+        System.out.println(ctx.getBean(PremierList.class).showAllPremier());
+        System.out.println(ctx.getBean(PremierList.class).showOnePremier("Опера2"));
         System.out.println(ctx.getBean(TicketService.class).buyTicket("Опера2", "2 ряд 1 место"));
         System.out.println(ctx.getBean(TicketService.class).returnTicket("Опера2", "2 ряд 1 место"));
     }
